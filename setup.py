85a5f68395e7e312826aae983c5489ca630909e2
Tao He <sighingnow@gmail.com>
2020-10-24 15:48:16 +0800

    Add readme.md to long description.

--- setup.py
+++ setup.py
@@ -49,12 +49,15 @@ class CopyNativeCommand(build_ext):
             self.copy_file(os.path.join(source_dir, libname),
                            os.path.join(target_dir, libname))
 
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8', mode='r') as fp:
+    long_description = fp.read()
 
 setup(
     name='libclang',
     version='11.0.0',
-    description='Clang Python Bindings.',
-    long_description='Clang Python Bindings, mirrored from the official LLVM repo: https://github.com/llvm/llvm-project/tree/master/clang/bindings/python, to make the installation process easier.',
+    description='Clang Python Bindings, mirrored from the official LLVM repo: https://github.com/llvm/llvm-project/tree/master/clang/bindings/python, to make the installation process easier.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Tao He',
     author_email='sighingnow@gmail.com',
     url='https://github.com/sighingnow/libclang',
