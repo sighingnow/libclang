9272df9c3a12cc6eeb8da9e83e8c1ddc8ccceacb
Tao He <sighingnow@gmail.com>
2021-07-22 11:21:08 +0800

    Fixes permission issue.
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- .github/script-to-build-alpine.sh
+++ .github/script-to-build-alpine.sh
@@ -8,5 +8,5 @@ sudo docker run --privileged --network=host --rm -v `pwd`:/work frolvlad/alpine-
                    cd /work/build && \
 					         cmake ../llvm -DLLVM_ENABLE_PROJECTS=clang -DBUILD_SHARED_LIBS=OFF -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_CXX_FLAGS_MINSIZEREL="-Os -DNDEBUG -static-libgcc -static-libstdc++ -s" && \
                    make libclang -j2'
-sudo chmod a+wr `pwd`/build
+sudo chmod -R a+wr `pwd`/build
 
