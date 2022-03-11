b5759033a33b4a68993d881da05222dd4e4a48fd
Tao He <sighingnow@gmail.com>
2022-03-11 13:08:51 +0800

    Build clang on arm/aarch64 using cross complilation, rather than qemu.
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- .github/script-to-build-aarch64.sh
+++ .github/script-to-build-aarch64.sh
@@ -3,8 +3,14 @@
 set -x
 
 sudo docker run --privileged --network=host --rm -v `pwd`:/work quay.io/pypa/manylinux2014_aarch64:latest \
-            sh -c 'mkdir -p /work/build && \
-                   cd /work/build && \
-					         cmake ../llvm -DLLVM_ENABLE_PROJECTS=clang -DBUILD_SHARED_LIBS=OFF -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_CXX_FLAGS_MINSIZEREL="-Os -DNDEBUG -static-libgcc -static-libstdc++ -s" && \
-                   make libclang -j2'
+  sh -c 'mkdir -p /work/build && \
+    cd /work/build && \
+    cmake ../llvm \
+      -DLLVM_ENABLE_PROJECTS=clang \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DLLVM_ENABLE_TERMINFO=OFF \
+      -DLLVM_TARGETS_TO_BUILD=AArch64 \
+      -DCMAKE_BUILD_TYPE=MinSizeRel \
+      -DCMAKE_CXX_FLAGS_MINSIZEREL="-Os -DNDEBUG -static-libgcc -static-libstdc++ -s" && \
+    make libclang -j2'
 sudo chmod -R a+wr `pwd`/build
