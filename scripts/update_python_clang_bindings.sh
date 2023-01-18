388c0e38a64d7cf932ca4594cade88aefb495a8a
Liu Jiaming <storypku@gmail.com>
2023-01-18 10:45:40 +0800

    Scripts: add script to help update python clang bindings (#42)

---
+++ scripts/update_python_clang_bindings.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+set -euo pipefail
+
+TOP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+
+LLVM_VERSION="${LLVM_VERSION:-15.0.6}"
+LLVM_URL_PREFIX="https://raw.githubusercontent.com/llvm/llvm-project/llvmorg-${LLVM_VERSION}/clang/bindings/python"
+
+LISTING=(
+  "clang/__init__.py"
+  "clang/cindex.py"
+  "clang/enumerations.py"
+)
+
+function run() {
+  echo >&2 "$*"
+  "$@"
+}
+
+BINDINGS_DIR="${TOP_DIR}/python"
+
+for clang_py_file in "${LISTING[@]}"; do
+  run curl -fsSL "${LLVM_URL_PREFIX}/${clang_py_file}" -o "${BINDINGS_DIR}/${clang_py_file}"
+done
+
+pushd "${TOP_DIR}" > /dev/null
+run patch -p1 < "${TOP_DIR}/scripts/data/clang_bindings.patch"
+popd > /dev/null
